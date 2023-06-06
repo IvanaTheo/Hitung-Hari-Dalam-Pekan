f77d9b66d1dfce9af3f2041aa8c70b45b9eae50e
kaa260503 <133626301+kaa260503@users.noreply.github.com>
2023-06-06 15:06:25 +0700

    Add files via upload

---
+++ Andhika.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package andhika;
+
+/**
+ *
+ * @author yahya
+ */
+public class Andhika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String nama = "Andhika Bayu Wahyu Wisnu";
+        String nim = "2113010271";
+
+        System.out.println("Nama: " + nama);
+        System.out.println("NIM: " + nim);
+    }
+    
+}
