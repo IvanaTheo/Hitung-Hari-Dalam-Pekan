00e71a38cb18199360004f565243caa31d888d45
Dabith45 <133625740+Dabith45@users.noreply.github.com>
2023-06-06 16:21:06 +0700

    Add files via upload

---
+++ Dabith_297.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author ACER
+ */
+public class Dabith_297 {
+    public static void main(String[] args) {
+        String nama="Dabith Hafithuddin";
+        String nim="2113010297";
+                
+        System.out.println("My Biodata saya");//mengeprint atau mencetak
+        System.out.println("----------------");
+        System.out.println("Nama \t\t\t\t:"+nama);
+        System.out.println("NIM \t\t\t\t:"+nim);
+    }
+    
+}
