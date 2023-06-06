4c461a76c21a605cb23f036fe46e06b29d9fb049
Sirkho <133625555+Sirkho@users.noreply.github.com>
2023-06-06 15:03:29 +0700

    Add files via upload

---
+++ ahmad_nim.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author HP
+ */
+public class ahmad_nim { 
+    public static void main(String[] args) {
+        System.out.println("nama : Ahmad Khozin Asrori Maarif");
+        System.out.println("nim : 2113010267");
+        System.out.println("kelas : 4G");
+       
+    }
+    
+}
