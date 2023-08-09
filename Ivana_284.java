ecedad8e04ddac73080f13fdc8373eac0a04bccf
IvanaTheo <133625553+IvanaTheo@users.noreply.github.com>
2023-08-09 13:29:38 +0700

    Revisi tugas UTS_284

---
+++ Ivana_284.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author User
+ */
+public class Ivana_284 {
+    public static void main(String[] args) {
+        String nama="Ivana Theophillia Oni Dawistri";
+        String nim="2113010284";
+                
+        System.out.println("Data Mahasiswa");//mengeprint atau mencetak
+        System.out.println("----------------");
+        System.out.println("Nama \t:"+nama);
+        System.out.println("NIM \t:"+nim);
+        
+    }
+}
